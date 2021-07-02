dd3a4282ee5d4d677b6f4f0afcfebd8f0373fb46
Andrew Boeckh <arb77@bath.ac.uk>
2021-07-02 17:55:46 -0400

    first commit

---
+++ takeTime.py
@@ -0,0 +1,11 @@
+from contextlib import contextmanager
+import time
+
+@contextmanager
+def takeTime(action):
+    takeTime.t0 = time.time()
+    print ('Elapsed time to %s...'% action)
+    yield
+    # print ('%.2f s' % (time.time() - takeTime.t0))
+    return time.time() - takeTime.t0
+
